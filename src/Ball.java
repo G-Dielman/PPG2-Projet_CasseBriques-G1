84022ea60a81b568c5e7c7af39c46c85dc024bfc
Jacob <0404jakomsa@student.epfc.eu>
2024-10-14 17:52:13 +0200

    Ajout de : -commentaires -Méthode "move" -Constructeur

--- src/Ball.java
+++ src/Ball.java
@@ -1,10 +1,17 @@
 public class Ball {
-    private double pX,pY,vX,vY,radius;
+    private double pX,pY,vX,vY,radius,move; //Initialisation des attributs de l'objet
 
-    public double getpX(){
+    public Ball(double pX,double pY,double vX,double vY){ //constructeur
+        this.pX = pX;
+        this.pY = pY;
+        this.vX = vX;
+        this.vY = vY;
+    }
+
+    public double getpX(){ //Getters de la position X
         return pX;
     }
-    public void setpX(double pX){
+    public void setpX(double pX){ //Setters del a position X
         this.pX = pX;
     }
     public double getpY(){
@@ -13,10 +20,10 @@ public class Ball {
     public void setpY(double pY){
         this.pY = pY;
     }
-    public double getvX(){
+    public double getvX(){ //getters Vitesse X
         return vX;
     }
-    public void setvX(double vX){
+    public void setvX(double vX){ //Setters Vitesse X
         this.vX = vX;
     }
     public double getvY(){
@@ -32,7 +39,10 @@ public class Ball {
         this.radius = radius;
     }
 
-
+    public void move(){
+        this.pX += this.vX;
+        this.pY += this.vY;
+    }
 
 
     
