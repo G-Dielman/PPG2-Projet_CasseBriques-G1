e80597840b479d4245deda44d97c357bb46ef2ec
Costi Gabor <1405cogabor@student.epfc.eu>
2024-12-10 17:15:26 +0100

    Creation de la classe abstraite GameObject et ses attributs

---
+++ src/GameObject.java
@@ -0,0 +1,13 @@
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
+
+public abstract class GameObject {
+
+    // Les attributs
+    private Shape shape;
+    private double x;
+    private double y;
+    private Color color;
+
+
+}
