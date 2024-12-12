a7e86f5ce9b21762da0f69738fc3f45453a4ec64
Jacob <0404jakomsa@student.epfc.eu>
2024-12-12 14:41:43 +0100

    Ajout du constructeur GameObject Ajout des Setter et Getter normal pour les méthodes Ajout des setter abstrait pour setX,setY,getShape

--- src/GameObject.java
+++ src/GameObject.java
@@ -9,5 +9,44 @@ public abstract class GameObject {
     private double y;
     private Color color;
 
+    public double getX() { //getter
+        return x;
+    }
 
+    public abstract void setX(double x); //setter abstrait
+
+    public double getY() { //getter
+        return y;
+    }
+
+    public abstract void setY(double y); //setter abstrait
+
+    public Color getColor() { //getter
+        return color;
+    }
+
+    public void setColor(Color color) { //setter
+        this.color = color;
+    }
+
+    public abstract Shape getShape(); //Getter abstrait
+
+    public void setShape(Shape shape) { //setter Shape
+        this.shape = shape;
+    }
+
+    public GameObject(double y, double x, Shape shape,Color color){ //constructeur
+        this.y = y;
+        this.x = x;
+        this.shape = shape;
+        this.color = color;
+    }
+
+    //Constructeur
+    //Cette classe doit contenir au moins un constructeur qui reçoit un Shape, deux doubles x et y et une
+    //couleur Color.
+    //• Méthodes
+    //Veillez à implémenter les accesseurs nécessaires pour les attributs créés ci-dessus. Notez toutefois que les
+    //Méthodes setX et setY doivent être abstraites, la façon de définir X et Y étant différente pour les
+    //Rectangles et pour les Circles. La méthode getShape() est elle aussi abstraite.
 }
