f636ef5a9bcfe15b16d5bc750be0df662ac29730
ayoub <0202ayoubboutuil@student.epfc.eu>
2024-12-16 19:41:31 +0100

    Creation de la classe abstraite brick avec son constructeur

---
+++ src/Brick.java
@@ -0,0 +1,28 @@
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
+
+
+public abstract class Brick extends RectangularGameObject {
+
+    protected boolean broken;
+    private boolean visible;
+
+
+    public Brick(double x, double y, double width, double height, Color color) {
+        super(x, y, width, height, color);
+
+    broken = false;
+    }
+
+    public boolean isBroken(){
+        return this.broken;
+    }
+
+    public void breakThis() {
+        broken = true;
+        this.shape.setVisible(false);
+    }
+
+
+}
