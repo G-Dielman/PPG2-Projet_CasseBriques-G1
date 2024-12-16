667a22b1e69d063c828d65ffc3b5a099af6eab84
ayoub <0202ayoubboutuil@student.epfc.eu>
2024-12-16 20:44:03 +0100

    methode de la classe SolidBrick;

--- src/SolidBrick.java
+++ src/SolidBrick.java
@@ -8,4 +8,13 @@ public class SolidBrick extends Brick{
         super(x, y, width, height, DEFAULT_COLOR);
         this.hits = 0;
     }
+
+    @Override
+    public void onCollisionWith(Ball ball) {
+        super.onCollisionWith(ball);
+        hits ++;
+         if (hits == 1) setColor(Color.GREEN);
+         if (hits == 2) setColor(Color.GREENYELLOW);
+         if(hits == 3) breakThis();
+    }
 }
